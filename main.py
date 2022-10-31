c20a5fee3147988b05c90c424a7a1ea8ad3c6a54
Jonathan Delgado <113931071+jrdelgado3@users.noreply.github.com>
2022-10-31 17:24:35 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+# from methods_help import method_help
+# from functions import function
+# from returnStuff import returnS
+from dynamic_functions import *
+# method_help()
+# function()
+# returnS()
+result = check_3Digits([55, 1500, 900, 150, 600])
+print(result)
